42797f978018611da3e8994757c34560dcbe775c
Bett-dev <bettkipkiruijustus@gmail.com>
2025-01-15 11:37:36 +0300

    list view and add task widget

--- lib/task_screen.dart
+++ lib/task_screen.dart
@@ -26,6 +26,9 @@ class TaskScreen extends StatelessWidget {
                       color: Colors.lightBlueAccent,
                     ),
                   ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
                   Text(
                     'Taskly',
                     style: TextStyle(
@@ -40,16 +43,142 @@ class TaskScreen extends StatelessWidget {
                 ],
               ),
             ),
-            Column(
-              children: [
-                Container(
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
                   color: Colors.white,
-                  height: 200,
-                )
-              ],
-            )
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListView(children: [
+                    TaskList(
+                      text: 'Task 1',
+                    ),
+                    TaskList(
+                      text: 'Task 2',
+                    ),
+                    TaskList(
+                      text: 'Task 3',
+                    ),
+                    TaskList(
+                      text: 'Task 4',
+                    ),
+                    TaskList(
+                      text: 'Task 5',
+                    ),
+                    TaskList(
+                      text: 'Task 6',
+                    ),
+                    TaskList(
+                      text: 'Task 7',
+                    ),
+                    TaskList(
+                      text: 'Task 2',
+                    ),
+                    TaskList(
+                      text: 'Task 3',
+                    ),
+                    TaskList(
+                      text: 'Task 4',
+                    ),
+                    TaskList(
+                      text: 'Task 5',
+                    ),
+                    TaskList(
+                      text: 'Task 12',
+                    ),
+                  ]),
+                ),
+              ),
+            ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet<void>(
+              context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) {
+                return SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        bottom: MediaQuery.of(context).viewInsets.bottom),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Text(
+                            textAlign: TextAlign.center,
+                            'Add Task',
+                            style: TextStyle(
+                                fontSize: 32, fontWeight: FontWeight.w600),
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.lightBlueAccent, width: 2),
+                              ),
+                              focusedBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.lightBlueAccent, width: 2),
+                              ),
+                            ),
+                            autofocus: true,
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStatePropertyAll(
+                                  Colors.lightBlueAccent),
+                            ),
+                            onPressed: () {},
+                            child: Text(
+                              'Submit',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              });
+        },
+        shape: CircleBorder(),
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(
+          Icons.add,
+          size: 30,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+  const TaskList({super.key, required this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        text,
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+      ),
+      trailing: Icon(
+        Icons.check_box_outline_blank,
+        size: 30,
       ),
     );
   }
